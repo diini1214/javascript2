2c61dd76ece24f21b6b49037d7d638bf35135163
diini1214 <71126396+diini1214@users.noreply.github.com>
2021-03-14 23:07:16 +0100

    Create 2.2.js

---
+++ 2.2.js
@@ -0,0 +1,13 @@
+ <script>
+            function multiplyBy() {
+                firstNumber = document.getElementById("firstNumber").value;
+                secondNumber = document.getElementById("secondNumber").value;
+                document.getElementById("result").innerHTML = firstNumber * secondNumber;
+            }
+    
+            function addBy() {
+                firstNumber = document.getElementById("firstNumber").value;
+                secondNumber = document.getElementById("secondNumber").value;
+                document.getElementById("result").innerHTML = firstNumber / secondNumber;
+            }
+        </script>
